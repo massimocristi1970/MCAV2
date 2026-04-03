4c66164b7fc949630beaa3748f4d9d022d8f46bb
massimocristi1970 <massimo@ticktockloans.com>
2026-04-03 19:50:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,13 +31,14 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Topic :: Office/Business :: Financial",
         "Topic :: Scientific/Engineering :: Information Analysis",
     ],
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     install_requires=requirements,
     extras_require={
         "dev": [
